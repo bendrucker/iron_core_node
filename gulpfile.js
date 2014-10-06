5b106e856d4af3b27a9e3c37d0e9799c1f1cd5f5
Ben Drucker <bvdrucker@gmail.com>
2014-10-06 06:59:52 +0200

    Run tests and generate a coverage report

--- gulpfile.js
+++ gulpfile.js
@@ -9,3 +9,16 @@ gulp.task('lint', function () {
     .pipe(plugins.jshint.reporter('jshint-stylish'))
     .pipe(plugins.jshint.reporter('fail'));
 });
+
+gulp.task('cover', function () {
+  return gulp.src('./src/**/*.js')
+    .pipe(plugins.istanbul());
+});
+
+gulp.task('test', ['cover'], function () {
+  return gulp.src('./test/specs/**/*.js')
+    .pipe(plugins.mocha())
+    .pipe(plugins.istanbul.writeReports({
+      reporters: ['text']
+    }));
+});
