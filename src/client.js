1ce3bb511fe9380737fe52d411edbc8151a1f084
Ben Drucker <bvdrucker@gmail.com>
2014-10-24 18:44:20 -0400

    Create version and header methods

--- src/client.js
+++ src/client.js
@@ -3,6 +3,7 @@
 var Promise     = require('bluebird');
 var needle      = Promise.promisifyAll(require('needle'));
 var createError = require('create-error');
+var pkg         = require('./pkg');
 
 function IronClient () {}
 
@@ -29,3 +30,11 @@ IronClient.prototype.request = function () {
   // arguments: method, url, data, options
   return request.apply(null, arguments);
 };
+
+IronClient.prototype.version = pkg.version
+
+Object.defineProperty(IronClient.prototype, 'headers', {
+  get: function () {
+    return 'iron-node/v' + this.version
+  }
+});
